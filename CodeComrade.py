d2198e28fb83b52a5276b9f26c80402c021a2994
fabi0323 <fabi3.meyer@gmail.com>
2024-03-08 08:49:29 +0100

    test

--- CodeComrade.py
+++ CodeComrade.py
@@ -1,2 +1,27 @@
 import discord as dc
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = dc.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
+
+
+
+
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == "neger":
+        message.channel.send == "sida"
+    
